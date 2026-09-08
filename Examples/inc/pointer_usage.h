f424c670d1c7887154986075b10ea0234fd2e7ea
Austin Harrison <systemverilogengineer@gmail.com>
2026-09-08 16:49:16 -0500

    more WIP pointer examples; for some reason I started with void pointers

--- Examples/inc/pointer_usage.h
+++ Examples/inc/pointer_usage.h
@@ -11,3 +11,30 @@
     #include "log.h"
   #endif /* POINTER_USAGE_H_ */
 #endif /* RUN==2*/
+
+/** @brief Not fully comprehensive because the goal is to show case common basics. */
+typedef enum{
+  E_INT8,
+  E_INT16,
+  E_INT32,
+  E_UINT8,
+  E_UINT16,
+  E_UINT32,
+  E_FLOAT,
+  E_DOUBLE
+} primitive_types_t;
+
+#define PU_LOG_INT8(X)        pu_print(E_INT8,   (void*)&X)
+#define PU_LOG_INT16(X)       pu_print(E_INT16,  (void*)&X)
+#define PU_LOG_INT32(X)       pu_print(E_INT32,  (void*)&X)
+#define PU_LOG_UINT8(X)       pu_print(E_UINT8,  (void*)&X)
+#define PU_LOG_UINT16(X)      pu_print(E_UINT16, (void*)&X)
+#define PU_LOG_UINT32(X)      pu_print(E_UINT32, (void*)&X)
+#define PU_LOG_FLOAT(X)       pu_print(E_FLOAT,  (void*)&X)
+#define PU_LOG_DOUBLE(X)      pu_print(E_DOUBLE, (void*)&X)
+
+
+/** @brief Casts a simple data type to void pointer */
+#define CAST_PRIMITIVE_TO_VOID_PTR(X)         (void*) &X
+
+void pu_print(primitive_types_t T, void* d);
--- Examples/src/pointer_usage.c
+++ Examples/src/pointer_usage.c
@@ -4,15 +4,63 @@
  * 
  */
 
+#include <stdint.h>
+#include <stdio.h>
+#include "pointer_usage.h"
 
 #if RUN==2
-    #include "pointer_usage.h"
     #include "log.h"
 
     int run (void) {
-      DELINEATE;
-      INFO("RUN#%0d -- TODO: POINTERS\n", RUN);
+
+      int8_t    var_int8_t     = (int8_t)   0x5a;
+      int16_t   var_int16_t    = (int16_t)  0xDEAD;
+      int32_t   var_int32_t    = (int32_t)  0xDEADBEEF;
+      uint8_t   var_uint8_t    = (uint8_t)  0x5A;
+      uint16_t  var_uint16_t   = (uint16_t) 0xDEAD;
+      uint32_t  var_uint32_t   = (uint32_t) 0xDEADBEEF;
+      float     var_float      = 3.14159f;
+      double    var_double     = 2.71828;
+
+      PU_LOG_INT8(var_int8_t);
+      PU_LOG_INT16(var_int16_t);
+      PU_LOG_INT32(var_int32_t);
+      PU_LOG_UINT8(var_uint8_t);
+      PU_LOG_UINT16(var_uint16_t);
+      PU_LOG_UINT32(var_uint32_t);
+      PU_LOG_FLOAT(var_float);
+      PU_LOG_DOUBLE(var_double);
       return 0;
     }
 
 #endif /* RUN==2*/
+
+/** @brief This function shall only be called with a helper macro that satisfies REG_EX: PU_LOG_\w+\d+ */
+void pu_print(primitive_types_t T, void* d) {
+  switch (T) {
+    case(E_INT8):
+      printf("Printing T=E_INT8    => %10d (decimal)  --  0x%08x (hex)\n", *(int8_t*)d, *(uint8_t*)d);
+      break;
+    case(E_INT16):
+      printf("Printing T=E_INT16   => %10d (decimal)  --  0x%08x (hex)\n", *(int16_t*)d, *(uint16_t*)d);
+      break;
+    case(E_INT32):
+      printf("Printing T=E_INT32   => %10d (decimal)  --  0x%08x (hex)\n", *(int32_t*)d, *(uint32_t*)d);
+      break;
+    case(E_UINT8):
+      printf("Printing T=E_UINT8   => %10d (decimal)  --  0x%08x (hex)\n", *(uint8_t*)d, *(uint8_t*)d);
+      break;
+    case(E_UINT16):
+      printf("Printing T=E_UINT16  => %10d (decimal)  --  0x%08x (hex)\n", *(uint16_t*)d, *(uint16_t*)d);
+      break;
+    case(E_UINT32):
+      printf("Printing T=E_UINT32  => %10d (decimal)  --  0x%08x (hex)\n", *(uint32_t*)d, *(uint32_t*)d);
+      break;
+    case(E_FLOAT):
+      printf("Printing T=E_FLOAT   => %2.7f\n", *((float*) d));
+      break;
+    case(E_DOUBLE):
+      printf("Printing T=E_DOUBLE  => %2.7f\n", *((double*) d));
+      break;
+  }
+}
